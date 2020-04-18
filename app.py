135cd00d0533b692e597445083bdc3afd018c426
vinzy619 <54163173+vinzy619@users.noreply.github.com>
2020-04-17 19:43:50 -0700

    deck of cards - print random card

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-print("Hello World")
+import random
 
 
+number = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "J", "Q", "K"]
+suite = ["spade", "club", "heart", "diamond"]
 
+deck = [v + " of " + w for v in number for w in suite]
+print(deck)
 
-
-
+print(random.choice(deck))
