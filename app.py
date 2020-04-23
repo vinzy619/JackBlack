9ab9ce65a4702d99e4721d463d8fab893510abcf
vinzy619 <54163173+vinzy619@users.noreply.github.com>
2020-04-23 09:52:10 -0700

    Add function hit_or_stand

--- app.py
+++ app.py
@@ -52,31 +52,60 @@ class Player:
         print("\nTotal:", total)
 
 
-def deal(deck, player):
-    dealer_hand = []
-    dealer_hand.append(deck.draw_card())
-    player.play_hand.append(deck.draw_card())
-    dealer_hand.append(deck.draw_card())
-    player.play_hand.append(deck.draw_card())
+class Dealer:
+    def __init__(self, deal_hand):
+        self.deal_hand = deal_hand
+
+    def show_dealer_hand(self):
+        total = 0
+        print("Dealer")
+        for cards in self.deal_hand:
+            print('{} of {}'.format(cards.value, cards.suite), end=", ")
+            total = total + cards.value
+
+        print("\nTotal:", total)
+
+
+def display(dealer, player):
     print("***************************************")
     print("Dealer")
-    print(dealer_hand[0].show()[0], "of", dealer_hand[0].show()[1])
+    print(dealer.deal_hand[0].show()[0], "of", dealer.deal_hand[0].show()[1])
     print("Total:", )
     print("***************************************")
     player.show_player_hand()
     print("***************************************")
 
 
+def hit_or_stand(player):
+    next_move = input("H: Hit, S: Stand, D: Double")
+    while next_move not in ["S", "s", "STAND", "Stand", "stand"]:
+        if next_move in ["H", "h", "HIT", "Hit", "hit"]:
+            player.play_hand.append(d1.draw_card())
+            display(deal1, p1)
+            return hit_or_stand(player)
+        if next_move in ["D", "d", "DOUBLE", "Double", "double"]:
+            player.play_hand.append(d1.draw_card())
+            display(deal1, p1)
+            break
+    else:
+        return
+
+
 if __name__ == '__main__':
     p1 = Player("John", play_hand=[])
+    deal1 = Dealer(deal_hand=[])
     d1 = Deck()
     d1.shuffle()
-    asktoplay = input("Do you wanna bet?")
-    if asktoplay in ["Y", "y", "YES", "Yes", "yes"]:
-        deal(d1, p1)
+    ask_to_play = input("Do you wanna bet?")
+    if ask_to_play in ["Y", "y", "YES", "Yes", "yes"]:
+        deal1.deal_hand.append(d1.draw_card())
+        p1.play_hand.append(d1.draw_card())
+        deal1.deal_hand.append(d1.draw_card())
+        p1.play_hand.append(d1.draw_card())
+        display(deal1, p1)
+        hit_or_stand(p1)
 
-    elif asktoplay in ["N", "n", "NO", "No", "no"]:
+    elif ask_to_play in ["N", "n", "NO", "No", "no"]:
         quit()
     else:
         pass
-
