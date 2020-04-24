400e022cc350722d6b968b75d3120b254863139e
vinzy619 <54163173+vinzy619@users.noreply.github.com>
2020-04-24 00:21:53 -0700

    Phase 1 of the project done

--- app.py
+++ app.py
@@ -46,41 +46,60 @@ class Player:
     def __init__(self, play_name, play_hand):
         self.name = play_name
         self.play_hand = play_hand
-        # self.total = 0
+        self.total = 0
 
     def show_player_hand(self):
-        self.total = 0
         print(self.name)
         for cards in self.play_hand:
             print('{} of {}'.format(cards.face, cards.suite), end=", ")
-            self.total = self.total + cards.value
-
-        print("\nTotal:", self.total)
 
 
 class Dealer:
     def __init__(self, deal_hand):
         self.deal_hand = deal_hand
-        # self.total = 0
+        self.total = 0
 
     def show_dealer_hand(self):
-        self.total = 0
         print("Dealer")
         for cards in self.deal_hand:
             print('{} of {}'.format(cards.face, cards.suite), end=", ")
-            self.total = self.total + cards.value
-
-        print("\nTotal:", self.total)
 
 
-def display(dealer, player):
-    print("***************************************")
-    print("Dealer")
-    print(dealer.deal_hand[0].show()[0], "of", dealer.deal_hand[0].show()[1])
-    print("Total:", )
+def hand_total(hand):
+    total = 0
+    for cards in hand:
+        total = total + cards.value
+    return total
+
+
+def display(player, call_stand):
+    if call_stand:
+        print("***************************************")
+        deal1.show_dealer_hand()
+        print("\nTotal:", hand_total(deal1.deal_hand))
+    else:
+        print("***************************************")
+        print("Dealer")
+        print(deal1.deal_hand[0].show()[0], "of", deal1.deal_hand[0].show()[1])
+        print("Total:", )
     print("***************************************")
     player.show_player_hand()
+    print("\nTotal:", hand_total(player.play_hand))
     print("***************************************")
+
+
+def stand(player):
+    while hand_total(deal1.deal_hand) < 17:
+        deal1.deal_hand.append(d1.draw_card())
+    display(player, call_stand=1)
+    if hand_total(deal1.deal_hand) > 21:
+        print("Dealer Bust", player.name, "Wins!!!")
+    elif hand_total(player.play_hand) > hand_total(deal1.deal_hand):
+        print(player.name, "Wins!!!")
+    elif hand_total(player.play_hand) < hand_total(deal1.deal_hand):
+        print("Dealer Wins!!!")
+    else:
+        print("Push!!!")
 
 
 def hit_or_stand(player):
@@ -88,21 +107,21 @@ def hit_or_stand(player):
     while next_move not in ["S", "s", "STAND", "Stand", "stand"]:
         if next_move in ["H", "h", "HIT", "Hit", "hit"]:
             player.play_hand.append(d1.draw_card())
-            display(deal1, player)
-            if player.total <= 21:
+            display(player, call_stand=0)
+            if hand_total(player.play_hand) <= 21:
                 return hit_or_stand(player)
             else:
                 print("Bust - You Loose")
                 break
         elif next_move in ["D", "d", "DOUBLE", "Double", "double"]:
             player.play_hand.append(d1.draw_card())
-            display(deal1, p1)
+            stand(player)
             break
         else:
             print("Invalid input. Please Try again.")
             next_move = input("H: Hit, S: Stand, D: Double")
     else:
-        print("Player Stands")
+        stand(player)
         return
 
 
@@ -117,7 +136,7 @@ if __name__ == '__main__':
         p1.play_hand.append(d1.draw_card())
         deal1.deal_hand.append(d1.draw_card())
         p1.play_hand.append(d1.draw_card())
-        display(deal1, p1)
+        display(p1, call_stand=0)
         hit_or_stand(p1)
 
     elif ask_to_play in ["N", "n", "NO", "No", "no"]:
